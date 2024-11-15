df296980c70e28e4a37425bb3d8adf51c01b9e3b
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2024-11-15 16:05:17 +0100

    add eigen as a git submodule

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Eigen/Dense>
 
 int main(int argc, char *argv[]) {
 
@@ -7,6 +8,14 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         std::cout << "Argument " << i << " = " << argv[i] << std::endl;
     }
+
+    // Tryout for Eigen library
+    Eigen::MatrixXd m(3, 3);
+    std::cout << "nb_rows: " << m.rows() << std::endl;
+    std::cout << "nb_cols: " << m.cols() << std::endl;
+    std::cout << "size: " << m.size() << std::endl;
+    std::cout << "m = " << m << std::endl;
+    std::cout << "m.T = " << m.transpose() << std::endl;
     
     return 0;
 }
