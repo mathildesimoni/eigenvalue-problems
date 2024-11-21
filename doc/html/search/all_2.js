0d3245f34dd6d396965079e2d1243d16d59801ac
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2024-11-21 12:38:07 +0100

    add documentation framework with Doxyfile and option to generate with Cmake

---
+++ doc/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['test_5fclass_2ecpp_0',['test_class.cpp',['../test__class_8cpp.html',1,'']]],
+  ['test_5fclass_2ehpp_1',['test_class.hpp',['../test__class_8hpp.html',1,'']]]
+];
---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['foo_0',['foo',['../test__class_8cpp.html#a14bcd2d544b22300bba3863251393e0f',1,'foo():&#160;test_class.cpp'],['../test__class_8hpp.html#a14bcd2d544b22300bba3863251393e0f',1,'foo():&#160;test_class.cpp']]]
+];
