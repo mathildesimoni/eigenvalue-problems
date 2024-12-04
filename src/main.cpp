b5941eb7afd76ca17552209a74c32ee1046eda42
chjulie <julie.charlet@epfl.ch>
2024-12-04 17:10:49 +0100

    adapt main.cpp structure

--- src/main.cpp
+++ src/main.cpp
@@ -14,69 +14,59 @@
 template <typename T>
 MatrixPointer<T> create_matrix(const std::string &input_name, const std::vector<std::string> &input_args)
 {
-    if (input_name == "function")
-    {
-        MatrixGeneratorFromFunction<T> generator(input_args);
-        MatrixPointer<T> matrix_pointer = generator.generate_matrix();
+    std::unique_ptr<MatrixGenerator<T>> generator;
+
+        if (input_name == "function")
+        {
+            generator = std::make_unique<MatrixGeneratorFromFunction<T>>(input_args);
+        }
+        else // input_name == "file"
+        {
+            generator = std::make_unique<MatrixGeneratorFromFile<T>>(input_args);
+        }
+
+        MatrixPointer<T> matrix_pointer = generator->generate_matrix();
         return matrix_pointer;
-    }
-    else // input_name == "file"
-    {
-        MatrixGeneratorFromFile<T> generator(input_args);
-        MatrixPointer<T> matrix_pointer = generator.generate_matrix();
-        return matrix_pointer;
-    }
 }
 
-// Instantiate the solver based on user args
-template <typename T>
-std::unique_ptr<AbstractIterativeSolver<T>> create_solver(const std::string &method_name, const std::vector<std::string> &method_args)
-{
-    if (method_name == "power_method")
-    {
-        auto solver = std::make_unique<PowerMethodSolver<T>>();
-        solver->SetMaxIter(std::stoi(method_args[0]));
-        solver->SetTolerance(std::stof(method_args[1]));
-        solver->SetShift(std::stof(method_args[2]));
-        return solver;
-    }
-    else if (method_name == "inverse_power_method")
-    {
-        auto solver = std::make_unique<InversePowerMethodSolver<T>>();
-        solver->SetMaxIter(std::stoi(method_args[0]));
-        solver->SetTolerance(std::stof(method_args[1]));
-        solver->SetShift(std::stof(method_args[2]));
-        return solver;
-    }
-    else // method_name = "QR_method"
-    {
-        auto solver = std::make_unique<QrMethodSolver<T>>();
-        solver->SetMaxIter(std::stoi(method_args[0]));
-        solver->SetTolerance(std::stof(method_args[1]));
-        return solver;
-    }
-}
 
 // Solve the eigenvalue problem
 template <typename T>
-void solve(const Config &config)
+Vector<T> solve_problem(const std::string &method_name, const std::vector<std::string> &method_args, MatrixPointer<T> matrix_pointer)
 {
-    // Matrix generation
-    auto matrix_pointer = create_matrix<T>(config.input.type, config.input.input_args);
-    std::cout << *matrix_pointer << std::endl;
-    std::cout << "Matrix pointer used to instantiate child class: " << matrix_pointer << std::endl;
-    std::cout << "Solving with: " << config.method.name << std::endl;
-    // TODO: remove above comments
+    std::unique_ptr<AbstractIterativeSolver<T>> solver;
+    if (method_name == "power_method")
+    {
+        auto power_solver = std::make_unique<PowerMethodSolver<T>>();
+        power_solver->SetShift(std::stof(method_args[2]));
+        solver = std::move(power_solver);
+    }
+    else if (method_name == "inverse_power_method")
+    {
+        auto inverse_power_solver = std::make_unique<InversePowerMethodSolver<T>>();
+        inverse_power_solver->SetShift(std::stof(method_args[2]));
+        solver = std::move(inverse_power_solver);
+    }x
+    else // method_name = "QR_method"
+    {
+        solver = std::make_unique<QrMethodSolver<T>>();
+    }
 
-    // Generate solver
-    auto solver = create_solver<T>(config.method.name, config.method.method_args);
+    solver->SetMaxIter(std::stoi(method_args[0]));
+    solver->SetTolerance(std::stof(method_args[1]));
     solver->SetMatrix(matrix_pointer);
 
     // Compute eigenvalues
     Vector<T> eigenvalues = solver->FindEigenvalues();
 
+    return eigenvalues;
+}
+
+template <typename T>
+void output_results(const std::string &output_type, const std::vector<std::string> &output_args, Vector<T> eigenvalues)
+{
     // Output eigenvalues
-    OutputGenerator<T> output_generator(config.output.type, config.output.output_args, eigenvalues);
+    OutputGenerator<T> output_generator(output_type, output_args, eigenvalues);
     output_generator.generate_output();
 }
 
@@ -166,7 +156,9 @@ int main(int argc, char *argv[])
             [&](auto &&chosen_type)
             {
                 using ChosenType = std::decay_t<decltype(chosen_type)>;
-                solve<ChosenType>(config);
+                MatrixPointer<ChosenType> matrix_pointer = create_matrix<ChosenType>(config.input.type, config.input.input_args);
+                Vector<ChosenType> eigenvalues = solve_problem<ChosenType>(config.method.name, config.method.method_args, matrix_pointer);
+                output_results<ChosenType>(config.output.type, config.output.output_args, eigenvalues);
             },
             variant_type);
     }
