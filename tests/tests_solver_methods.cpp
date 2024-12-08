a930fb9f320c68bcf99d70c94af9635d73319a96
chjulie <julie.charlet@epfl.ch>
2024-12-08 11:59:55 +0100

    write tests for QrDecomposition method

--- tests/tests_solver_methods.cpp
+++ tests/tests_solver_methods.cpp
@@ -108,7 +108,7 @@ TEST(inverse_power_method_solver, hilbert_matrix){
         1.0 / 3.0, 1.0 / 4.0, 1.0 / 5.0;
 
     // Instantiate solver
-    PowerMethodSolver<type_test> solver;
+    InversePowerMethodSolver<type_test> solver;
     solver.SetTolerance(1e-6);
     solver.SetMaxIter(1000);
     solver.SetShift(0.0);
@@ -131,7 +131,7 @@ TEST(qr_method_solver, hilbert_matrix){
         1.0 / 3.0, 1.0 / 4.0, 1.0 / 5.0;
     
     // Instantiate solver
-    PowerMethodSolver<type_test> solver;
+    QrMethodSolver<type_test> solver;
     solver.SetTolerance(1e-6);
     solver.SetMaxIter(1000);
     solver.SetMatrix(hilbert);
@@ -146,3 +146,89 @@ TEST(qr_method_solver, hilbert_matrix){
 }
 
 // EXPECT_THROW for square matrix test
+
+TEST(qr_decomposition, hilbert_matrix){
+
+    auto hilbert = std::make_shared<Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic>>(3, 3);
+    *hilbert << 1.0, 1.0 / 2.0, 1.0 / 3.0,
+        1.0 / 2.0, 1.0 / 3.0, 1.0 / 4.0,
+        1.0 / 3.0, 1.0 / 4.0, 1.0 / 5.0;
+
+    // Instantiate solver
+    QrMethodSolver<type_test> solver;
+    solver.SetTolerance(1e-6);
+    solver.SetMaxIter(1000);
+    solver.SetMatrix(hilbert);
+
+    int n = (*hilbert).rows();
+
+    // Initialize Q and R
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> Q(n, n);
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> R(n, n);
+
+    solver.QrDecomposition(*hilbert, Q, R); 
+
+    // Test that R is upper diagonal
+    bool is_upper_triangular = true;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (std::abs(R(i, j)) > 1e-6) {
+                is_upper_triangular = false;
+                break;
+            }
+        }
+    }
+    EXPECT_TRUE(is_upper_triangular);
+
+    // Test that Q is orthogonal
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> identity = Q.transpose() * Q;
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> expected_identity = Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic>::Identity(n, n);
+    EXPECT_TRUE(identity.isApprox(expected_identity, 1e-6));
+
+    // Test the QR = *hilbert
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> reconstructed_hilbert = Q * R;
+    EXPECT_TRUE(reconstructed_hilbert.isApprox(*hilbert, 1e-6));
+}
+
+TEST(qr_decomposition, identity_matrix){
+
+    auto identity = std::make_shared<Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic>>(3, 3);
+    *identity << 1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0;
+
+    // Instantiate solver
+    QrMethodSolver<type_test> solver;
+    solver.SetTolerance(1e-6);
+    solver.SetMaxIter(1000);
+    solver.SetMatrix(identity);
+
+    int n = (*identity).rows();
+
+    // Initialize Q and R
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> Q(n, n);
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> R(n, n);
+
+    solver.QrDecomposition(*identity, Q, R); 
+
+    // Test that R is upper diagonal
+    bool is_upper_triangular = true;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (std::abs(R(i, j)) > 1e-6) {
+                is_upper_triangular = false;
+                break;
+            }
+        }
+    }
+    EXPECT_TRUE(is_upper_triangular);
+
+    // Test that Q is orthogonal
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> identity_reformed = Q.transpose() * Q;
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> expected_identity = Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic>::Identity(n, n);
+    EXPECT_TRUE(identity_reformed.isApprox(expected_identity, 1e-6));
+
+    // Test the QR = *hilbert
+    Eigen::Matrix<type_test, Eigen::Dynamic, Eigen::Dynamic> reconstructed_identity = Q * R;
+    EXPECT_TRUE(reconstructed_identity.isApprox(*identity, 1e-6));
+}
