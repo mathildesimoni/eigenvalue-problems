a1b7a755b41b797afe0882a5b0b40211119cdf99
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2024-12-12 11:10:29 +0100

    catch invalid arguments for the solver and handle it by setting default values

--- src/SolverFactory.cpp
+++ src/SolverFactory.cpp
@@ -10,37 +10,47 @@ std::unique_ptr<AbstractIterativeSolver<T>> SolverFactory<T>::ChooseSolver()
     if (methodArgs.size() > 3)
         throw std::invalid_argument("Expected maximum 3 arguments (tolerance, maximum iterations and shift), but got " + std::to_string(methodArgs.size()));
     // Declare and initialize methodArgs with default values
-    double tolerance = 1e-6;
-    int maxIter = 10000;
-    float shift = 0.0;
+    double tolerance = DefaultSolverArgs::TOLERANCE;
+    int maxIter = DefaultSolverArgs::MAX_ITER;
+    float shift = DefaultSolverArgs::SHIFT;
 
-    // Check tolerance input, set tolerance = 1e-6 if no arguments
+    // Check tolerance input, set tolerance = 1e-6 if no argument or invalue argument
     if (methodArgs.size() > 0)
     {
         try
         {
-            tolerance = std::stod(methodArgs[0]);
+            double toleranceTmp = std::stod(methodArgs[0]);
+            if (toleranceTmp <= 0.0)
+                throw std::invalid_argument("The tolerance must be a positive number, but got " + std::to_string(toleranceTmp));
+            tolerance = toleranceTmp;
         }
         catch (std::invalid_argument &e)
         {
-            std::cerr << "Error processing the argument 'tolerance': " << e.what() << std::endl;
+            std::cerr << "[WARNING] Error processing the argument 'tolerance': " << e.what() << std::endl
+                      << "          Handling the issue by setting 'tolerance' to the default value (" << std::to_string(tolerance) << ")"
+                      << std::endl;
         }
     }
 
-    // Check maxIter input, set maxIter = 10000 if no arguments
+    // Check maxIter input, set maxIter = 10000 if no arguments or invalid argument
     if (methodArgs.size() > 1)
     {
         // Check max iterations input
         try
         {
-            maxIter = std::stoi(methodArgs[1]);
+            int maxIterTmp = std::stoi(methodArgs[1]);
+            if (maxIterTmp <= 0.0)
+                throw std::invalid_argument("The maximum number of iterations must be a positive integer, but got " + std::to_string(maxIterTmp));
+            maxIter = maxIterTmp;
         }
         catch (std::invalid_argument &e)
         {
-            std::cerr << "Error processing the argument 'maxIter': " << e.what() << std::endl;
+            std::cerr << "[WARNING] Error processing the argument 'maxIter': " << e.what() << std::endl
+                      << "          Handling the issue by setting 'maxIter' to the default value (" << std::to_string(maxIter) << ")"
+                      << std::endl;
         }
     }
-    // Check shift input, set shift = 0.0 if no arguments
+    // Check shift input, set shift = 0.0 if no arguments or invalid argument
     if (methodArgs.size() > 2)
     {
         try
@@ -49,7 +59,9 @@ std::unique_ptr<AbstractIterativeSolver<T>> SolverFactory<T>::ChooseSolver()
         }
         catch (std::invalid_argument &e)
         {
-            std::cerr << "Error processing the argument 'shift': " << e.what() << std::endl;
+            std::cerr << "[WARNING] Error processing the argument 'shift': " << e.what() << std::endl
+                      << "          Handling the issue by setting 'shift' to the default value (" << std::to_string(shift) << ")"
+                      << std::endl;
         }
     }
 
@@ -69,7 +81,8 @@ std::unique_ptr<AbstractIterativeSolver<T>> SolverFactory<T>::ChooseSolver()
     }
     else
     {
-        throw std::invalid_argument("Unsupported method: " + methodName);
+        throw std::runtime_error(methodName + " is a supported method but is not linked to a valid implementation.\n"
+                                              "Consider updating the SolverFactory to consider this method");
     }
     return solver;
 }
